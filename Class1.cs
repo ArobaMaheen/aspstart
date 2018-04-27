c174b296ea3eb9a46ef13a9dac36caf6feb62374
Aroba Maheen <arobamaheen@gmail.com>
2018-04-28 01:45:36 +0500

    firstcommit

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    public class Class1
+    {
+        public double ans;
+        public double add(double a,double b)
+        {
+            ans = a + b;
+            return ans;
+        }
+        public double sub(double a, double b)
+        {
+            ans = a - b;
+            return ans;
+        }
+        public double mult(double a, double b)
+        {
+            ans = a * b;
+            return ans;
+        }
+        public double div(double a, double b)
+        {
+            if(b!=0){
+            ans = a / b;
+           
+            }
+            else
+            {
+
+            }
+            return ans;
+        }
+    }
+}
